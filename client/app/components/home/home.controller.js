fea1014debb9c66c0fcbbb032f64896bf7d0d7d5
Ryan Ford <ryno412@gmail.com>
2017-03-25 15:51:35 -0700

    add some flexbox

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -1,14 +1,53 @@
 import appIconSrc from '../../assets/imgs/appIcon.png';
 import analyticsIconSrc from '../../assets/imgs/analyticsIcon.png';
+import salesAndTrendsIconSrc from '../../assets/imgs/salesTrendsIcon.png';
+import paymentsAndReportsIconSrc from '../../assets/imgs/paymentsIcon.png';
+import userIconSrc from '../../assets/imgs/userRolesIcon.png';
+import bankIconSrc from '../../assets/imgs/taxBankingIcon.png';
+import helpIconSrc from '../../assets/imgs/helpIcon.png';
 
 class HomeController {
-  constructor($rootScope) {
+  constructor(/*$rootScope*/) {
     this.name = 'home';
-    this.myAppsIcon = appIconSrc;
-    this.anaylticsIcon = analyticsIconSrc;
-    this.$rootScope = $rootScope;
-    this.$rootScope.bodyClazz = 'FOO'
   }
+
+  icons = [
+    {
+      name: 'My Apps',
+      iconSrc: appIconSrc,
+      link: 'myapps'
+    },
+    {
+      name: 'Anaylitics',
+      iconSrc: analyticsIconSrc,
+      link: 'myapps'
+    },
+    {
+      name: 'Sales And Trends',
+      iconSrc: salesAndTrendsIconSrc,
+      link: 'myapps'
+    },
+    {
+      name: 'Payments and Financial Reports',
+      iconSrc: paymentsAndReportsIconSrc,
+      link: 'myapps'
+    },
+    {
+      name: 'Users and Roles',
+      iconSrc: userIconSrc,
+      link: 'myapps'
+    },
+    {
+      name: 'Aggrements, Tax, and Banking',
+      iconSrc: bankIconSrc,
+      link: 'myapps'
+    },
+    {
+      name: 'Resources and Help',
+      iconSrc: helpIconSrc,
+      link: 'myapps'
+    }
+  ]
 }
-HomeController.$inject = ['$rootScope'];
+//HomeController.$inject = ['$rootScope'];
 export default HomeController;
