6c634f37ea2638ac0f90e986d79c870ca80509fa
Ryan Ford <ryno412@gmail.com>
2017-03-27 22:31:48 -0700

    user info with coverage link

--- client/app/app.controller.js
+++ client/app/app.controller.js
@@ -4,6 +4,7 @@
 class AppController {
   constructor($transitions,) {
     this.name = 'app';
+    // add page name to body class for style overrides on route change;
     $transitions.onStart({}, (trans)=>{
       const fromName = `${trans.$from().name}-page`;
       const toName = `${trans.$to().name}-page`;
